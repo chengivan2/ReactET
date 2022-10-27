9242747628a80f42b41ba30855a49df73b9ee008
Ivan Cheng'ole <chengoleivan@gmail.com>
2022-10-27 10:11:24 +0300

    Firt element created using createelement

--- reactet/src/App.js
+++ reactet/src/App.js
@@ -1,13 +1,22 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const elementOne = React.createElement(
+  'h1',
+  {className: 'greeting'},
+  'Ivan is Awesomer!'
+);
+
 function App() {
   if (20 > 30) {
-    return <h1> Hello World!</h1>
+    return <h1> Hello World!</h1>;
   }
   else {
-    return <h2>Hello Correct World!</h2>
+    return elementOne;
   }
 }
+
+
 
 export default App;
