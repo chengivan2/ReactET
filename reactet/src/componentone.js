c9e2845dae0cfc93f6a9577cb624cf3ee5ab512a
Ivan Cheng'ole <chengoleivan@gmail.com>
2022-10-27 10:51:34 +0300

    Class component created

--- reactet/src/componentone.js
+++ reactet/src/componentone.js
@@ -1,3 +1,5 @@
+//This is a function component
+
 function Welcome(props) {
     return <h1>Hello, {props.name}</h1>;
 }
--- reactet/src/index.js
+++ reactet/src/index.js
@@ -3,24 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Welcome from './componentone';
+import Welcome2 from './componenttwo'
 import reportWebVitals from './reportWebVitals';
 
-const element = <React.StrictMode>
-                  <Welcome name="Sara" />
-                </React.StrictMode>;
+const element1 =  <React.StrictMode>
+                    <Welcome name="Sara" />
+                  </React.StrictMode>;
+
+const element2 =  <React.StrictMode>
+                    <Welcome2 name="Ivan" />
+                  </React.StrictMode>;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-    {element}
+    {element1}
+    {element2}
   </div>
 );
-
-const root2 = ReactDOM.createRoot(document.getElementsByClassName('id'));
-
-root2.render(element);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
